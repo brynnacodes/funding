045d323801b730d11c37c7084446a656438f6a85
Brynna McCarter and Dan Lauby <brynna@>
2017-04-05 09:35:04 -0700

    Seed firebase and install angularefire/update module

---
+++ src/app/project.model.ts
@@ -0,0 +1,4 @@
+export class Project {
+
+constructor(public title: string, public author: string, public description: string, public author: string, public image: string, public totalFunding: number, public currentFunding: number, public categories: string[]) {}
+}
